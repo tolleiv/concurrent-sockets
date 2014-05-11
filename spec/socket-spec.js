11eee6279e559f3796bc538aa5e1f18dc57238cc
Tolleiv Nietsch <info@tolleiv.de>
2014-05-11 12:13:30 +0200

    Add some socket action along with few tests

---
+++ spec/socket-spec.js
@@ -0,0 +1,47 @@
+var helper = require('./spec-helper');
+
+
+describe("a running webserver", function () {
+
+    beforeEach(helper.start);
+    afterEach(helper.stop);
+
+    describe("passing data through the socket", function () {
+        var request = helper.req;
+
+        beforeEach(helper.connectSocket);
+        afterEach(helper.disconnect);
+
+        describe("using the main route", function () {
+
+            it("responds ok and sends data back through socket", function (done) {
+                helper.socket().on("request", function (data) {
+                    expect(data).toEqual('demodemo');
+                    done();
+                });
+
+                request.get("/demodemo", function (err, res, body) {
+                    expect(err).toBeNull();
+                    expect(res.statusCode).toEqual(200);
+                });
+            });
+        });
+
+        describe("with a Redis publisher", function () {
+            var redis = require('redis');
+            var pub = redis.createClient();
+
+            it("passes Strings through as JSON", function (done) {
+
+                helper.socket().on("notify", function (data) {
+                    expect(data).toEqual(jasmine.any(Object));
+                    expect(data.my).toEqual('demodata');
+                    done();
+                });
+
+                pub.publish("events", '{"event":"notify","data":{"my":"demodata"}}');
+
+            });
+        });
+    });
+});
--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,38 +1,47 @@
 var http = require('http');
 var request = require("request");
+var io = require('socket.io-client');
 
-var server, app, started = false;
+var server, socket, app;
 
 exports.req = {
     get: function (path, callback) {
-        return request({url: "http://127.0.0.1:3001" + path, json:true}, callback);
+        return request({url: "http://127.0.0.1:3001" + path, json: true}, callback);
     }
 };
 
-var startServer = function (env) {
+exports.start = function (cb) {
     app = require("../lib/app.js").app;
-    app.set('env', env);
-
+    app.set('env', 'test');
     app.set('events', function() { });
 
     server = http.createServer(app);
+    require('../lib/socket')(server, app)
     server.listen(3001)
         .on('listening', function () {
-            setTimeout(function () {
-                started = true;
-            }, 100)
+            setTimeout(cb, 100)
         })
-        .on('close', function () {
-            started = false;
-        });
-};
-
-exports.start = startServer.bind(null, 'test')
-
-exports.isStarted = function () {
-    return started;
 };
 
 exports.stop = function (cb) {
     server.close(cb);
+};
+
+exports.connectSocket = function (cb) {
+    socket = io.connect('http://localhost:3001', {
+        'reconnection delay': 0, 'reopen delay': 0, 'force new connection': true
+    });
+    socket.on('connect', function () {
+        setTimeout(cb, 100)
+    });
+};
+
+exports.disconnect = function (cb) {
+    socket.on('disconnect', function() { setTimeout(cb, 100) });
+    socket.disconnect();
+};
+
+
+exports.socket = function () {
+    return socket;
 };
